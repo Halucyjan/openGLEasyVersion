d9e1781da578728106fe0115625bf3c384d9ee1d
Halucyjan2 <250179@student.pwr.edu.pl>
2024-03-25 22:57:29 +0100

    camera header

---
+++ Camera.cpp
@@ -0,0 +1,12 @@
+#include "Camera.h"
+
+Camera::Camera(int width, int height, glm::vec3 position) {
+
+}
+
+void Camera::Matrix(float FOVdeg, float nearPlane, float farPlane, Shader& shader, const char* uniform) {
+
+}
+void Camera::Inputs(GLFWwindow* window) {
+
+}
