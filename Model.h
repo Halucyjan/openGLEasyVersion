59364dbc89e0c6e4b2dfe5e5e428623b5445f25b
Halucyjan2 <250179@student.pwr.edu.pl>
2024-04-22 01:19:17 +0200

    model class part 1

--- Model.h
+++ Model.h
@@ -18,6 +18,12 @@ private:
 	json JSON;
 
 	std::vector<unsigned char> getData();
+	std::vector<float> getFloats(json accessor);
+	std::vector<GLuint> getIndices(json accessor);
+
+	std::vector<glm::vec2> groupFloatsVec2(std::vector<float> floatVec);
+	std::vector<glm::vec3> groupFloatsVec3(std::vector<float> floatVec);
+	std::vector<glm::vec4> groupFloatsVec4(std::vector<float> floatVec);
 };
 
 #endif MODEL_CLASS_H
